aab2d312de377b7bfd279a964aae6dae64dcc6de
Lee <leecook808@gmail.com>
2020-10-29 23:36:48 -0700

    Spawner Serialize Support

--- src/main/java/com/cryptomorin/xseries/XItemStack.java
+++ src/main/java/com/cryptomorin/xseries/XItemStack.java
@@ -148,6 +148,9 @@ public final class XItemStack {
                         i++;
                     }
                 }
+            } else if (state instanceof CreatureSpawner) {
+                CreatureSpawner cs = (CreatureSpawner) state;
+                config.set("spawner", cs.getSpawnedType().name());
             }
         } else if (meta instanceof EnchantmentStorageMeta) {
             EnchantmentStorageMeta book = (EnchantmentStorageMeta) meta;
