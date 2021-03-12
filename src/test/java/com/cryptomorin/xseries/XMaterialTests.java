2308166986ecc021d4d591aede73ab370dfc32f0
Hasan Demirtaş <utsukushihito@outlook.com>
2021-03-12 23:25:04 +0300

    improve performance.

--- src/test/java/com/cryptomorin/xseries/XMaterialTests.java
+++ src/test/java/com/cryptomorin/xseries/XMaterialTests.java
@@ -2,10 +2,12 @@ package com.cryptomorin.xseries;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 final class XMaterialTests extends TestSpigot {
 
     @Test
-    void testXMaterial() throws InterruptedException {
+    void testXMaterial() throws InterruptedException, IOException {
         runServer(() -> {
             System.out.println(XMaterial.getVersion());
         });
