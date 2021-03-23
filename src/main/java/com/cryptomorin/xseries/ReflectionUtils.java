9b2c8073551a68e152f104ccabc6bf0576e6655f
Crypto Morin <cryptomorin@gmail.com>
2021-03-23 01:28:17 -0700

    v7.9.1
    Pushed some changes that were missing in the last version.

--- src/main/java/com/cryptomorin/xseries/ReflectionUtils.java
+++ src/main/java/com/cryptomorin/xseries/ReflectionUtils.java
@@ -29,6 +29,7 @@ import javax.annotation.Nullable;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -41,9 +42,11 @@ import java.util.concurrent.CompletableFuture;
  * <a href="https://wiki.vg/Protocol">Clientbound Packets</a> are considered fake
  * updates to the client without changing the actual data. Since all the data is handled
  * by the server.
+ * <p>
+ * A useful resource used to compare mappings is <a href="https://minidigger.github.io/MiniMappingViewer/#/spigot">Mini's Mapping Viewer</a>
  *
  * @author Crypto Morin
- * @version 2.0.0
+ * @version 3.0.0
  */
 public class ReflectionUtils {
     /**
@@ -53,13 +56,31 @@ public class ReflectionUtils {
      * E.g. EntityPlayer in 1.15 is in the class {@code net.minecraft.server.v1_15_R1}
      * but in 1.14 it's in {@code net.minecraft.server.v1_14_R1}
      * In order to maintain cross-version compatibility we cannot import these classes.
+     * <p>
+     * Performance is not a concern for these specific statically initialized values.
      */
-    public static final String VERSION = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-    public static final String CRAFTBUKKIT = "org.bukkit.craftbukkit." + VERSION + '.';
-    public static final String NMS = "net.minecraft.server." + VERSION + '.';
+    public static final String
+            VERSION = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3],
+            CRAFTBUKKIT = "org.bukkit.craftbukkit." + VERSION + '.',
+            NMS = "net.minecraft.server." + VERSION + '.';
 
+    /**
+     * A nullable public accessible field only available in {@code EntityPlayer}.
+     * This can be null if the player is offline.
+     */
     private static final MethodHandle PLAYER_CONNECTION;
+    /**
+     * Responsible for getting the NMS handler {@code EntityPlayer} object for the player.
+     * {@code CraftPlayer} is simply a wrapper for {@code EntityPlayer}.
+     * Used mainly for handling packet related operations.
+     * <p>
+     * This is also where the famous player {@code ping} field comes from!
+     */
     private static final MethodHandle GET_HANDLE;
+    /**
+     * Sends a packet to the player's client through a {@code NetworkManager} which
+     * is where {@code ProtocolLib} controls packets by injecting channels!
+     */
     private static final MethodHandle SEND_PACKET;
 
     static {
@@ -84,8 +105,7 @@ public class ReflectionUtils {
         GET_HANDLE = getHandle;
     }
 
-    private ReflectionUtils() {
-    }
+    private ReflectionUtils() {}
 
     /**
      * Get a NMS (net.minecraft.server) class.
@@ -145,6 +165,29 @@ public class ReflectionUtils {
             }
         } catch (Throwable throwable) {
             throwable.printStackTrace();
+        }
+    }
+
+    @Nullable
+    public static Object getHandle(@Nonnull Player player) {
+        Objects.requireNonNull(player, "Cannot get handle of null player");
+        try {
+            return GET_HANDLE.invoke(player);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            return null;
+        }
+    }
+
+    @Nullable
+    public static Object getConnection(@Nonnull Player player) {
+        Objects.requireNonNull(player, "Cannot get connection of null player");
+        try {
+            Object handle = GET_HANDLE.invoke(player);
+            return PLAYER_CONNECTION.invoke(handle);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            return null;
         }
     }
 
