cc8ff33a5b8bdefbb863d54fca5a28f8ee8dc91d
timderspieler <timderspieler@gmx.de>
2023-12-21 17:55:00 +0100

    Add backwards compatibility

--- src/main/java/com/cryptomorin/xseries/NMSExtras.java
+++ src/main/java/com/cryptomorin/xseries/NMSExtras.java
@@ -558,7 +558,8 @@ public final class NMSExtras {
         Location location = chest.getLocation();
         try {
             Object world = WORLD_HANDLE.invoke(location.getWorld());
-            Object position = BLOCK_POSITION.invoke(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+            Object position = v(19, BLOCK_POSITION.invoke(location.getBlockX(), location.getBlockY(), location.getBlockZ()))
+                    .orElse(BLOCK_POSITION.invoke(location.getX(), location.getY(), location.getZ()));
             Object block = GET_BLOCK.invoke(GET_BLOCK_TYPE.invoke(world, position));
             PLAY_BLOCK_ACTION.invoke(world, position, block, 1, open ? 1 : 0);
         } catch (Throwable throwable) {
