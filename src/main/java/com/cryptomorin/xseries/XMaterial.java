88a278cad46424e141ff417ecb350fd59e1ab94e
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2024-10-13 09:28:02 +0200

    Fix NPE in clinit

--- src/main/java/com/cryptomorin/xseries/XMaterial.java
+++ src/main/java/com/cryptomorin/xseries/XMaterial.java
@@ -2314,11 +2314,15 @@ public enum XMaterial /* implements com.cryptomorin.xseries.abstractions.Materia
         private static final int VERSION;
 
         static { // This needs to be right below VERSION because of initialization order.
-            String version = Bukkit.getVersion();
-            Matcher matcher = Pattern.compile("MC: \\d\\.(\\d+)").matcher(version);
+            if (Bukkit.getServer() == null) {
+                VERSION = 21;
+            } else {
+                String version = Bukkit.getVersion();
+                Matcher matcher = Pattern.compile("MC: \\d\\.(\\d+)").matcher(version);
 
-            if (matcher.find()) VERSION = Integer.parseInt(matcher.group(1));
-            else throw new IllegalArgumentException("Failed to parse server version from: " + version);
+                if (matcher.find()) VERSION = Integer.parseInt(matcher.group(1));
+                else throw new IllegalArgumentException("Failed to parse server version from: " + version);
+            }
         }
 
         /**
