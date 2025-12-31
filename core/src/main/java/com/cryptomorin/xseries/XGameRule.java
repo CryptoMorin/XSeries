20c6b64205a124c265ee8f940e3f4eea63e9e41d
AlmightySatan <43804777+Almighty-Satan@users.noreply.github.com>
2025-12-31 12:00:26 +0100

    Make fields final

--- core/src/main/java/com/cryptomorin/xseries/XGameRule.java
+++ core/src/main/java/com/cryptomorin/xseries/XGameRule.java
@@ -68,7 +68,7 @@ public final class XGameRule<T> implements XBase<XGameRule<T>, String> {
         World_setGameRuleValue = setGameRuleValue;
     }
 
-    public static XGameRule<Boolean> COMMAND_BLOCK_OUTPUT = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "command_block_output", "commandBlockOutput"),
+    public static final XGameRule<Boolean> COMMAND_BLOCK_OUTPUT = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "command_block_output", "commandBlockOutput"),
             ADVANCE_TIME = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "advance_time", "doDaylightCycle"),
             ENTITY_DROPS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "entity_drops", "doEntityDrops"),
             MOB_DROPS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "mob_drops", "doMobLoot"),
@@ -81,108 +81,108 @@ public final class XGameRule<T> implements XBase<XGameRule<T>, String> {
             REDUCED_DEBUG_INFO = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "reduced_debug_info", "reducedDebugInfo"),
             SEND_COMMAND_FEEDBACK = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "send_command_feedback", "sendCommandFeedback"),
             SHOW_DEATH_MESSAGES = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "show_death_messages", "showDeathMessages");
-    public static XGameRule<Integer> RANDOM_TICK_SPEED = new XGameRule<>(Integer.class, /* v1.21.11+ */ "random_tick_speed", "randomTickSpeed");
+    public static final XGameRule<Integer> RANDOM_TICK_SPEED = new XGameRule<>(Integer.class, /* v1.21.11+ */ "random_tick_speed", "randomTickSpeed");
 
     @XInfo(since = "1.9")
-    public static XGameRule<Boolean> SPECTATORS_GENERATE_CHUNKS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spectators_generate_chunks", "spectatorsGenerateChunks"),
+    public static final XGameRule<Boolean> SPECTATORS_GENERATE_CHUNKS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spectators_generate_chunks", "spectatorsGenerateChunks"),
             ELYTRA_MOVEMENT_CHECK = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "elytra_movement_check", "disableElytraMovementCheck");
     @XInfo(since = "1.9")
-    public static XGameRule<Integer> RESPAWN_RADIUS = new XGameRule<>(Integer.class, /* v1.21.11+ */ "respawn_radius", "spawnRadius");
+    public static final XGameRule<Integer> RESPAWN_RADIUS = new XGameRule<>(Integer.class, /* v1.21.11+ */ "respawn_radius", "spawnRadius");
 
     @XInfo(since = "1.11")
-    public static XGameRule<Boolean> ADVANCE_WEATHER = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "advance_weather", "doWeatherCycle");
+    public static final XGameRule<Boolean> ADVANCE_WEATHER = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "advance_weather", "doWeatherCycle");
     @XInfo(since = "1.11")
-    public static XGameRule<Integer> MAX_ENTITY_CRAMMING = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_entity_cramming", "maxEntityCramming");
+    public static final XGameRule<Integer> MAX_ENTITY_CRAMMING = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_entity_cramming", "maxEntityCramming");
 
     @XInfo(since = "1.12")
-    public static XGameRule<Boolean> LIMITED_CRAFTING = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "limited_crafting", "doLimitedCrafting"),
+    public static final XGameRule<Boolean> LIMITED_CRAFTING = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "limited_crafting", "doLimitedCrafting"),
             SHOW_ADVANCEMENT_MESSAGES = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "show_advancement_messages", "announceAdvancements");
     @XInfo(since = "1.12")
-    public static XGameRule<Integer> MAX_COMMAND_SEQUENCE_LENGTH = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_command_sequence_length", "maxCommandChainLength");
+    public static final XGameRule<Integer> MAX_COMMAND_SEQUENCE_LENGTH = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_command_sequence_length", "maxCommandChainLength");
 
     @XInfo(since = "1.12", removedSince = "1.13")
-    public static XGameRule<String> GAME_LOOP_FUNCTION = new XGameRule<>(String.class, "gameLoopFunction");
+    public static final XGameRule<String> GAME_LOOP_FUNCTION = new XGameRule<>(String.class, "gameLoopFunction");
 
     @XInfo(since = "1.14.3")
-    public static XGameRule<Boolean> RAIDS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "raids", "disableRaids");
+    public static final XGameRule<Boolean> RAIDS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "raids", "disableRaids");
 
     @XInfo(since = "1.15")
-    public static XGameRule<Boolean> SPAWN_PHANTOMS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spawn_phantoms", "doInsomnia"),
+    public static final XGameRule<Boolean> SPAWN_PHANTOMS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spawn_phantoms", "doInsomnia"),
             IMMEDIATE_RESPAWN = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "immediate_respawn", "doImmediateRespawn"),
             DROWNING_DAMAGE = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "drowning_damage", "drowningDamage"),
             FALL_DAMAGE = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "fall_damage", "fallDamage"),
             FIRE_DAMAGE = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "fire_damage", "fireDamage");
 
     @XInfo(since = "1.15.2")
-    public static XGameRule<Boolean> SPAWN_PATROLS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spawn_patrols", "doPatrolSpawning"),
+    public static final XGameRule<Boolean> SPAWN_PATROLS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spawn_patrols", "doPatrolSpawning"),
             SPAWN_WANDERING_TRADERS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spawn_wandering_traders", "doTraderSpawning");
 
     @XInfo(since = "1.16.1")
-    public static XGameRule<Boolean> FORGIVE_DEAD_PLAYERS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "forgive_dead_players", "forgiveDeadPlayers"),
+    public static final XGameRule<Boolean> FORGIVE_DEAD_PLAYERS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "forgive_dead_players", "forgiveDeadPlayers"),
             UNIVERSAL_ANGER = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "universal_anger", "universalAnger");
 
     @XInfo(since = "1.17")
-    public static XGameRule<Boolean> FREEZE_DAMAGE = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "freeze_damage", "freezeDamage");
+    public static final XGameRule<Boolean> FREEZE_DAMAGE = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "freeze_damage", "freezeDamage");
     @XInfo(since = "1.17")
-    public static XGameRule<Integer> PLAYERS_SLEEPING_PERCENTAGE = new XGameRule<>(Integer.class, /* v1.21.11+ */ "players_sleeping_percentage", "playersSleepingPercentage");
+    public static final XGameRule<Integer> PLAYERS_SLEEPING_PERCENTAGE = new XGameRule<>(Integer.class, /* v1.21.11+ */ "players_sleeping_percentage", "playersSleepingPercentage");
 
     @XInfo(since = "1.19")
-    public static XGameRule<Boolean> SPAWN_WARDENS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spawn_wardens", "doWardenSpawning");
+    public static final XGameRule<Boolean> SPAWN_WARDENS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spawn_wardens", "doWardenSpawning");
 
     @XInfo(since = "1.19.3")
-    public static XGameRule<Boolean> BLOCK_EXPLOSION_DROP_DECAY = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "block_explosion_drop_decay", "blockExplosionDropDecay"),
+    public static final XGameRule<Boolean> BLOCK_EXPLOSION_DROP_DECAY = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "block_explosion_drop_decay", "blockExplosionDropDecay"),
             MOB_EXPLOSION_DROP_DECAY = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "mob_explosion_drop_decay", "mobExplosionDropDecay"),
             TNT_EXPLOSION_DROP_DECAY = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "tnt_explosion_drop_decay", "tntExplosionDropDecay"),
             WATER_SOURCE_CONVERSION = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "water_source_conversion", "waterSourceConversion"),
             LAVA_SOURCE_CONVERSION = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "lava_source_conversion", "lavaSourceConversion"),
             GLOBAL_SOUND_EVENTS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "global_sound_events", "globalSoundEvents");
     @XInfo(since = "1.19.3")
-    public static XGameRule<Integer> MAX_SNOW_ACCUMULATION_HEIGHT = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_snow_accumulation_height", "snowAccumulationHeight");
+    public static final XGameRule<Integer> MAX_SNOW_ACCUMULATION_HEIGHT = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_snow_accumulation_height", "snowAccumulationHeight");
 
     @XInfo(since = "1.19.4")
-    public static XGameRule<Boolean> SPREAD_VINES = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spread_vines", "doVinesSpread");
+    public static final XGameRule<Boolean> SPREAD_VINES = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spread_vines", "doVinesSpread");
     @XInfo(since = "1.19.4")
-    public static XGameRule<Integer> MAX_BLOCK_MODIFICATIONS = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_block_modifications", "commandModificationBlockLimit");
+    public static final XGameRule<Integer> MAX_BLOCK_MODIFICATIONS = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_block_modifications", "commandModificationBlockLimit");
 
     @XInfo(since = "1.20.2")
-    public static XGameRule<Boolean> ENDER_PEARLS_VANISH_ON_DEATH = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "ender_pearls_vanish_on_death", "enderPearlsVanishOnDeath");
+    public static final XGameRule<Boolean> ENDER_PEARLS_VANISH_ON_DEATH = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "ender_pearls_vanish_on_death", "enderPearlsVanishOnDeath");
 
     @XInfo(since = "1.20.3")
-    public static XGameRule<Boolean> PROJECTILES_CAN_BREAK_BLOCKS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "projectiles_can_break_blocks", "projectilesCanBreakBlocks");
+    public static final XGameRule<Boolean> PROJECTILES_CAN_BREAK_BLOCKS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "projectiles_can_break_blocks", "projectilesCanBreakBlocks");
     @XInfo(since = "1.20.3")
-    public static XGameRule<Integer> MAX_COMMAND_FORKS = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_command_forks", "maxCommandForkCount"),
+    public static final XGameRule<Integer> MAX_COMMAND_FORKS = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_command_forks", "maxCommandForkCount"),
             PLAYERS_NETHER_PORTAL_DEFAULT_DELAY = new XGameRule<>(Integer.class, /* v1.21.11+ */ "players_nether_portal_default_delay", "playersNetherPortalDefaultDelay"),
             PLAYERS_NETHER_PORTAL_CREATIVE_DELAY = new XGameRule<>(Integer.class, /* v1.21.11+ */ "players_nether_portal_creative_delay", "playersNetherPortalCreativeDelay");
 
     @XInfo(since = "1.20.5", removedSince = "1.21.9")
-    public static XGameRule<Integer> SPAWN_CHUNK_RADIUS = new XGameRule<>(Integer.class, "spawnChunkRadius");
+    public static final XGameRule<Integer> SPAWN_CHUNK_RADIUS = new XGameRule<>(Integer.class, "spawnChunkRadius");
 
     @XInfo(since = "1.21.2")
-    public static XGameRule<Boolean> PLAYER_MOVEMENT_CHECK = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "player_movement_check", "disablePlayerMovementCheck");
+    public static final XGameRule<Boolean> PLAYER_MOVEMENT_CHECK = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "player_movement_check", "disablePlayerMovementCheck");
     @XInfo(since = "1.21.2")
-    public static XGameRule<Integer> MAX_MINECART_SPEED = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_minecart_speed", "minecartMaxSpeed");
+    public static final XGameRule<Integer> MAX_MINECART_SPEED = new XGameRule<>(Integer.class, /* v1.21.11+ */ "max_minecart_speed", "minecartMaxSpeed");
 
     @XInfo(since = "1.21.5")
-    public static XGameRule<Boolean> TNT_EXPLODES = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "tnt_explodes", "tntExplodes");
+    public static final XGameRule<Boolean> TNT_EXPLODES = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "tnt_explodes", "tntExplodes");
 
     @XInfo(since = "1.21.5", removedSince = "1.21.11")
-    public static XGameRule<Boolean> ALLOW_FIRE_TICKS_AWAY_FROM_PLAYER = new XGameRule<>(Boolean.class, "allowFireTicksAwayFromPlayer");
+    public static final XGameRule<Boolean> ALLOW_FIRE_TICKS_AWAY_FROM_PLAYER = new XGameRule<>(Boolean.class, "allowFireTicksAwayFromPlayer");
 
     @XInfo(since = "1.21.6")
-    public static XGameRule<Boolean> LOCATOR_BAR = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "locator_bar", "locatorBar");
+    public static final XGameRule<Boolean> LOCATOR_BAR = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "locator_bar", "locatorBar");
 
     @XInfo(since = "1.21.9")
-    public static XGameRule<Boolean> PVP = new XGameRule<>(Boolean.class, "pvp"),
+    public static final XGameRule<Boolean> PVP = new XGameRule<>(Boolean.class, "pvp"),
             ALLOW_ENTERING_NETHER_USING_PORTALS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "allow_entering_nether_using_portals", "allowEnteringNetherUsingPortals"),
             SPAWN_MONSTERS = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spawn_monsters", "spawnMonsters"),
             COMMAND_BLOCKS_WORK = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "command_blocks_work", "commandBlocksEnabled"),
             SPAWNER_BLOCKS_WORK = new XGameRule<>(Boolean.class, /* v1.21.11+ */ "spawner_blocks_work", "spawnerBlocksEnabled");
 
     @XInfo(since = "1.21.11")
-    public static XGameRule<Integer> FIRE_SPREAD_RADIUS_AROUND_PLAYER = new XGameRule<>(Integer.class, "fire_spread_radius_around_player");
+    public static final XGameRule<Integer> FIRE_SPREAD_RADIUS_AROUND_PLAYER = new XGameRule<>(Integer.class, "fire_spread_radius_around_player");
 
     @XInfo(since = "?", removedSince = "1.21.11")
-    public static XGameRule<Boolean> DO_FIRE_TICK = new XGameRule<>(Boolean.class, "doFireTick");
+    public static final XGameRule<Boolean> DO_FIRE_TICK = new XGameRule<>(Boolean.class, "doFireTick");
 
     private final String[] names;
     private final Class<?> type;
